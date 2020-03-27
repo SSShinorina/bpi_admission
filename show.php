40ad738a82cab1247fbfe084acbb2114a76a6fa5
SSShinorina <53445219+SSShinorina@users.noreply.github.com>
2020-03-27 22:19:50 +0600

    class roll auto generate

--- show.php
+++ show.php
@@ -1,11 +1,10 @@
 <?php
 include_once ("../src/Admission.php");
 $obj1=new Admission();
-$obj1->setData($_GET);
-$item=$obj1->show();
-
+$items=$obj1->show();
 ?>
 <html>
+
 
 <table border="1">
     <tr>
@@ -55,55 +54,59 @@ $item=$obj1->show();
         <td>Referred Student Mobile</td>
 
     </tr>
+<?php
+foreach ($items as $data){
 
+    ?>
 
     <tr>
-        <td><?php echo $item['id'] ?></td>
-        <td><?php echo $item['a_fee'] ?></td>
-        <td><?php echo $item['sem_fee'] ?></td>
-        <td><?php echo $item['a_date'] ?></td>
-        <td><?php echo $item['c_roll']?></td>
-        <td><?php echo $item['st_nm_bangla'] ?></td>
-        <td><img src="<?php echo $item['u_photo']?>"></td>
-        <td><?php echo $item['st_nm'] ?></td>
-        <td><?php echo $item['f_nm'] ?></td>
-        <td><?php echo $item['m_nm'] ?></td>
-        <td><?php echo $item['ssc_roll'] ?></td>
-        <td><?php echo $item['ssc_reg'] ?></td>
-        <td><?php echo $item['ssc_year'] ?></td>
-        <td><?php echo $item['ssc_gpa'] ?></td>
-        <td><?php echo $item['tech'] ?></td>
-        <td><?php echo $item['a_approval'] ?></td>
-        <td><?php echo $item['u_id'] ?></td>
-        <td><?php echo $item['a_id'] ?></td>
-        <td><?php echo $item['gender'] ?></td>
-        <td><?php echo $item['dob'] ?></td>
-        <td><?php echo $item['address'] ?></td>
-        <td><?php echo $item['street'] ?></td>
-        <td><?php echo $item['city'] ?></td>
-        <td><?php echo $item['state'] ?></td>
-        <td><?php echo $item['postal'] ?></td>
-        <td><?php echo $item['country'] ?></td>
-        <td><?php echo $item['st_number'] ?></td>
-        <td><?php echo $item['f_number'] ?></td>
-        <td><?php echo $item['m_number'] ?></td>
-        <td><?php echo $item['g_number'] ?></td>
-        <td><?php echo $item['ssc_board'] ?></td>
-        <td><?php echo $item['pp_photo'] ?></td>
-        <td><img src="<?php echo $item['u_testimonial']?>"></td>
-        <td><img src="<?php echo $item['u_mark']?>"></td>
-        <td><?php echo $item['remark'] ?></td>
-        <td><?php echo $item['remark_pay'] ?></td>
-        <td><?php echo $item['payment_id'] ?></td>
-        <td><?php echo $item['a_institute'] ?></td>
-        <td><?php echo $item['reffered_by'] ?></td>
-        <td><?php echo $item['ref_st_nm'] ?></td>
-        <td><?php echo $item['ref_st_tech'] ?></td>
-        <td><?php echo $item['ref_st_roll'] ?></td>
-        <td><?php echo $item['ref_st_session'] ?></td>
-        <td><?php echo $item['ref_st_mobile'] ?></td>
-
+        <td><?php echo $data['id'] ?></td>
+        <td><?php echo $data['a_fee'] ?></td>
+        <td><?php echo $data['sem_fee'] ?></td>
+        <td><?php echo $data['a_date'] ?></td>
+        <td><?php echo $data['c_roll'] ?></td>
+        <td><?php echo $data['st_nm_bangla'] ?></td>
+        <td><img src="<?php echo $data['u_photo']?>"></td>
+        <td><?php echo $data['st_nm'] ?></td>
+        <td><?php echo $data['f_nm'] ?></td>
+        <td><?php echo $data['m_nm'] ?></td>
+        <td><?php echo $data['ssc_roll'] ?></td>
+        <td><?php echo $data['ssc_reg'] ?></td>
+        <td><?php echo $data['ssc_year'] ?></td>
+        <td><?php echo $data['ssc_gpa'] ?></td>
+        <td><?php echo $data['tech'] ?></td>
+        <td><?php echo $data['a_approval'] ?></td>
+        <td><?php echo $data['u_id'] ?></td>
+        <td><?php echo $data['a_id'] ?></td>
+        <td><?php echo $data['gender'] ?></td>
+        <td><?php echo $data['dob'] ?></td>
+        <td><?php echo $data['address'] ?></td>
+        <td><?php echo $data['street'] ?></td>
+        <td><?php echo $data['city'] ?></td>
+        <td><?php echo $data['state'] ?></td>
+        <td><?php echo $data['postal'] ?></td>
+        <td><?php echo $data['country'] ?></td>
+        <td><?php echo $data['st_number'] ?></td>
+        <td><?php echo $data['f_number'] ?></td>
+        <td><?php echo $data['m_number'] ?></td>
+        <td><?php echo $data['g_number'] ?></td>
+        <td><?php echo $data['ssc_board'] ?></td>
+        <td><?php echo $data['pp_photo'] ?></td>
+        <td><img src="<?php echo $data['u_testimonial']?>"></td>
+        <td><img src="<?php echo $data['u_mark']?>"></td>
+        <td><?php echo $data['remark'] ?></td>
+        <td><?php echo $data['remark_pay'] ?></td>
+        <td><?php echo $data['payment_id'] ?></td>
+        <td><?php echo $data['a_institute'] ?></td>
+        <td><?php echo $data['reffered_by'] ?></td>
+        <td><?php echo $data['ref_st_nm'] ?></td>
+        <td><?php echo $data['ref_st_tech'] ?></td>
+        <td><?php echo $data['ref_st_roll'] ?></td>
+        <td><?php echo $data['ref_st_session'] ?></td>
+        <td><?php echo $data['ref_st_mobile'] ?></td>
     </tr>
+    
+    <?php } ?>
 
 </table>
 
