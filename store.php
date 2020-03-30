49cdd4d0acc5036303bbf13a870b8780c9cb6ec3
SSShinorina <53445219+SSShinorina@users.noreply.github.com>
2020-03-30 23:24:45 +0600

    roll number auto increament

--- store.php
+++ store.php
@@ -1,12 +1,28 @@
 <?php
 include_once("../src/Admission.php");
-//print_r($_POST);
-$obj = new Admission();
-$obj->setData($_POST);
-$obj->store();
-print_r($_POST);
+
+//$obj = new Admission();
+//$obj->setData($_POST);
+//$obj->store();
+//if (isset($_POST['technology'])) {
+//    $class_roll = $_POST['technology'] . "001" . '<br>';
+//    print_r($class_roll);
+//}
+////print_r($_POST);
+ 
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+        foreach ($_POST['technology'] as $roll) {
+//            $_POST['c_roll'] = 001;
+//            $_POST['c_roll'] = ++$_POST['c_roll'];
+            $_POST['c_roll'] = $roll['technology'] . $_POST['c_roll'];
+        }
+            $obj = new Admission();
+            $obj->setData($_POST);
+            $obj->store();
+
+}
+
 echo "Successfully added. To see details go to.<a href='show.php'>Click Here</a>";
-
-
-
-
